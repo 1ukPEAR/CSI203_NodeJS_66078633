528eb677badb08bf93b8a37f9ae5ee4d08661c73
1ukPEAR <pairryinter@gmail.com>
2025-02-08 15:05:48 +0700

    Second commit NodeJS

--- startNode.js
+++ startNode.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('CSI203: DIGITAL ARCHITECTURE AND OPERATING SYSTEM\nThis is a sample learning tool for the CSI 203 course. | BY: Thararat Sinsom 66078633');
+    res.end('CSI203: DIGITAL ARCHITECTURE AND OPERATING SYSTEM\nThis is a sample learning tool for the CSI 203 course. | BY: Thararat Sinsom 66078633 Section(L)004 )');
 });
 
 server.listen(port, hostname, () => {
